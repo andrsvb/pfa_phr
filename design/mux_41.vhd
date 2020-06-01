65921242c1bd8b8da29f680e41abf7d8ed2171ff
andrsvb <39677537+andrsvb@users.noreply.github.com>
2020-06-01 15:09:26 +0200

    Update mux_41.vhd

--- design/mux_41.vhd
+++ design/mux_41.vhd
@@ -41,20 +41,16 @@ end mux_41;
 
 architecture Behavioral of mux_41 is
 
-begin
-process (A,B,C,D,S0,S1) is
-begin
-  if (S0 ='0' and S1 = '0') then
-      Z <= A;
-  elsif (S0 ='1' and S1 = '0') then
-      Z <= B;
-  elsif (S0 ='0' and S1 = '1') then
-      Z <= C;
-  else
-      Z <= D;
-  end if;
- 
-end process;
+signal auxSelVect : std_logic_vector (1 downto 0);
 
+begin
+
+auxSelVect(1) <= s1;
+auxSelVect(0) <= s0;
+
+z <= a when auxSelVect = "00" else
+     b when auxSelVect = "01" else
+     c when auxSelVect = "10" else
+     d when auxSelVect = "11";
 
 end Behavioral;
