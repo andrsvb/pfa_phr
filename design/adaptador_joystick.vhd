25fdd7b7a6584b5ef45d17c6accc22f5b5f37e40
andrsvb <39677537+andrsvb@users.noreply.github.com>
2020-06-06 14:55:05 +0200

    añadido comentarios a los adaptadores

--- design/adaptador_joystick.vhd
+++ design/adaptador_joystick.vhd
@@ -40,9 +40,12 @@ entity adaptador_joystick is
         down : out std_logic
 	);
 end adaptador_joystick;
+-- se comunica con el componente creado con el xadc wizard para obtener la posicion del joystick
+--      y lo saca transformado en dos señales: up y down
 
 architecture Behavioral of adaptador_joystick is
 
+-- componente generado por el wizard
 component xadc_wiz_0 is
    port
    (
@@ -66,35 +69,35 @@ component xadc_wiz_0 is
 );
 end component;
 
-signal adc_high : std_logic_vector (7 downto 0);
-signal adc_low : std_logic_vector (7 downto 0);
-signal enableint : std_logic := '1';
+signal adc_high : std_logic_vector (7 downto 0);        -- parte mas significativa del valor recibido por el adc
+signal adc_low : std_logic_vector (7 downto 0);         -- parte menos significativa del valor recibido por el adc
+signal enableint : std_logic := '1';                    -- siguiendo el tutorial, para que funcione en 1
 
 begin
 
 xadc : xadc_wiz_0 
     port map(
-    daddr_in        => "0010011",
-    den_in          => enableint,
-    di_in           => (others => '0'),
-    dwe_in          => '0',
-    do_out (15 downto 8)   => adc_high,
-    do_out (7 downto 0)    => adc_low,
-    drdy_out        => open,
-    dclk_in         => clk,
-    reset_in        => reset,
-    vauxp0          => adc,
-    vauxn0          => '0',
-    busy_out        => open,
-    channel_out     => open,
-    eoc_out         => enableint,
-    eos_out         => open,
-    alarm_out       => open,
-    vp_in           => '0',
-    vn_in           => '0'
+    daddr_in        => "0010011",                       --direccion del registro del puerto A13
+    den_in          => enableint,                       -- para que funcione en 1
+    di_in           => (others => '0'),                 -- a 0 porq no lo usamos
+    dwe_in          => '0',                             -- a 0 porq no lo usamos
+    do_out (15 downto 8)   => adc_high,                 -- pasamos la parte mas significativa
+    do_out (7 downto 0)    => adc_low,                  -- pasamos la parte menos significativa
+    drdy_out        => open,                            -- abierto porq no lo usamos
+    dclk_in         => clk,                             -- reloj de la Nexys, de 100 MHz
+    reset_in        => reset,                           -- señal de reset
+    vauxp0          => adc,                             -- puerto conectado al U/D de joystick
+    vauxn0          => '0',                             -- a 0 porq no lo usamos
+    busy_out        => open,                            -- abierto porq no lo usamos
+    channel_out     => open,                            -- abierto porq no lo usamos
+    eoc_out         => enableint,                       -- para que funcione en 1
+    eos_out         => open,                            -- abierto porq no lo usamos
+    alarm_out       => open,                            -- abierto porq no lo usamos
+    vp_in           => '0',                             -- a 0 porq no lo usamos
+    vn_in           => '0'                              -- a 0 porq no lo usamos
 );
 
-up <= '1' when adc_high = "11111111" else '0';
-down <= '1' when adc_high = "00000000" else '0';
+up <= '1' when adc_high = "11111111" else '0';          -- cuando el joystick esta subido ponemos up a 1, si no esta a 0
+down <= '1' when adc_high = "00000000" else '0';        -- cuando el joystick esta bajado ponemos down a 1, si no esta a 0
 
 end Behavioral;
--- design/adaptador_uart.vhd
+++ design/adaptador_uart.vhd
@@ -33,59 +33,59 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity adaptador_uart is
     Port ( 
-           clk : in STD_LOGIC;
-           reset : in STD_LOGIC;
-           puntuacion : in STD_LOGIC_VECTOR (9 downto 0);
-           rx : in STD_LOGIC;
-           tx : out STD_LOGIC
+           clk : in STD_LOGIC;                                  -- señal de reloj
+           reset : in STD_LOGIC;                                -- señal de reset
+           puntuacion : in STD_LOGIC_VECTOR (9 downto 0);       -- puntuacion en 10 bits
+           rx : in STD_LOGIC;                                   -- pin rx
+           tx : out STD_LOGIC                                   -- pin tx
     );
 end adaptador_uart;
 
 architecture Behavioral of adaptador_uart is
 
-signal enable : std_logic := '0';                   --
-signal tx_data : std_logic_vector (7 downto 0);     --
-signal tx_busy : std_logic;                         --
-signal rx_data : std_logic_vector (7 downto 0);     --no lo usamos ya que no recibimos
-signal rx_busy : std_logic;                         --^^
-signal rx_error : std_logic;                        --^^
-signal contador : std_logic := '0';                 --para saber si hemos mandado la primera parte del mensaje o la segunda
+signal enable : std_logic := '0';                    -- señal enable para mandar mensajes
+signal tx_data : std_logic_vector (7 downto 0);      -- datos a mandar
+signal tx_busy : std_logic;                          -- señal busy de mandar mensajes
+signal rx_data : std_logic_vector (7 downto 0);      -- no lo usamos ya que no recibimos
+signal rx_busy : std_logic;                          -- ^^
+signal rx_error : std_logic;                         -- ^^
+signal contador : std_logic := '0';                  -- para saber si hay que mandar la primera parte del mensaje o la segunda
 
 begin
 
 uart : ENTITY work.uart
 	GENERIC MAP(
-		clk_freq	=> 100_000_000,
-		baud_rate	=> 19_200,
-		os_rate		=> 16,
-		d_width		=> 8,
-		parity		=> 1,
-		parity_eo	=> '0'
+		clk_freq	=> 100_000_000,                  -- frecuencia de reloj de la Nexys: 100 MHz
+		baud_rate	=> 19_200,                       -- baudios con los que queremos que trabaje la uart
+		os_rate		=> 16,                           -- oversampling
+		d_width		=> 8,                            -- tamaño del bus
+		parity		=> 1,                            -- paridad
+		parity_eo	=> '0'                           -- paridad even
 	)
 	PORT MAP(
-		clk		=> clk,
-		reset_n	=> reset,
-		tx_ena	=> enable,
-		tx_data	=> tx_data,
-		rx		=> rx,
-		rx_busy	=> rx_busy,
-		rx_error => rx_error,
-		rx_data	=> rx_data,
-		tx_busy	=> tx_busy,
-		tx		=> tx
+		clk		=> clk,                              -- señal de reloj de la Nexys
+		reset_n	=> reset,                            -- señal reset
+		tx_ena	=> enable,                           -- señal enable
+		tx_data	=> tx_data,                          -- datos a mandar
+		rx		=> rx,                               -- pin rx
+		rx_busy	=> rx_busy,                          -- no lo usamos ya que no recibimos
+		rx_error => rx_error,                        -- ^^
+		rx_data	=> rx_data,                          -- ^^
+		tx_busy	=> tx_busy,                          -- señal busy de mandar mensajes
+		tx		=> tx                                -- pin tx
     );
     
-uart_proc : process (puntuacion)
+uart_proc : process (puntuacion)        -- como la puntuacion es de 10 bits y el bus de 8, hay que mandarla en dos mensajes con 0s para rellenar
 begin
         if(tx_busy = '0') then
-            if(contador = '0') then
-                tx_data (7 downto 2) <= "000000";
-                tx_data (1 downto 0) <= puntuacion (9 downto 8);
-            else
-                tx_data (7 downto 0) <= puntuacion (7 downto 0);
+            if(contador = '0') then                         -- mandamos la primera parte
+                tx_data (7 downto 2) <= "000000";                   -- relleno
+                tx_data (1 downto 0) <= puntuacion (9 downto 8);    -- primeros dos bits de puntuacion
+            else                                            -- mandamos la segunda parte
+                tx_data (7 downto 0) <= puntuacion (7 downto 0);    -- ultimos 8 bits de puntuacion
             end if;
-            contador <= NOT contador;
-            enable <= '1';
+            contador <= NOT contador;       -- para enviar las partes correctamente
+            enable <= '1';                  -- para mandar el mensaje
         end if;
         if(enable = '1') then
             enable <= '0';
